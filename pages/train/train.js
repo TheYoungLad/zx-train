68f500a97daf78857b7892191f38d68c58544a21
Phoenix <123456>
2019-09-27 16:33:50 +0800

    完成首页页面

--- pages/train/train.js
+++ pages/train/train.js
@@ -5,15 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
+    /**
+     * 海报轮播初始数据
+     */
     indicatorDots:false,
     autoplay:"true",
     interval:"5000",
     duration:"1000",
     imgUrls:[
       '/images/haibao/1.jpg',
-      '/images/haibao/1.jpg',
+      '/images/haibao/2.jpg',
       '/images/haibao/3.jpg'
     ],
+    /**
+     * currentTab默认值为0
+     */
     currentTab:0
   },
 
@@ -21,24 +27,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /**
+     * 页面初始化 options为页面跳转所带来的参数
+     */
   },
 
-  // switchNav:function(e){
-  //   var id = e.currentTarget.id;
-  //   this.setData({currentTab: id});
-  // },
-
-  // formSubmit:function(e) {
-  //   console.log(e);
-  //   var startStation = e.detail.value.startStation;  //始发站
-  //   var endStation = e.detail.value.endStation;  //始发站
-  //   var date = e.detail.value.date;   //日期2月2日
-  //   var week = e.detail.value.week;   //星期：周四
-  //   wx.navigateTo({
-  //     url: '../trainList/trainList?startStation='+startStation +"&endStartStation="+endStation+"&date="+date+"&week"+week
-  //   })
-  // },
+/**
+ * switchNav进行页签的切换，动态改变变量currentTab的值
+ */
+  switchNav:function(e){
+    var id = e.currentTarget.id;
+    this.setData({currentTab: id});
+  },
+/**
+ * 表单提交formSubmit事件，将始发站，终点站，日期，星期带到trainList火车票列表界面
+ ，在这个火车票查询界面中输入始发站和终点站，单击“查询”按钮，就会根据输入的内容进行火车票列表的查询*/
+  formSubmit:function(e) {
+    console.log(e);
+    var startStation = e.detail.value.startStation;  //始发站
+    var endStation = e.detail.value.endStation;  //终点站
+    var date = e.detail.value.date;   //日期2月2日
+    var week = e.detail.value.week;   //星期：周四
+    wx.navigateTo({
+      url: '../trainList/trainList?startStation='+startStation +"&endStation="+endStation+"&date="+date+"&week"+week
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
--- pages/trainList/trainList.js
+++ pages/trainList/trainList.js
@@ -1,18 +1,73 @@
 // pages/trainList/trainList.js
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    date:'',
+    /**
+     * trainList变量用来绑定火车票列表信息
+     * winHeight变量动态计算列表信息
+     */
+    trainList:[],
+    winHeight:600,
+    /**
+     * 定义currentTab动态渲染样式
+     */
+    currentTab:'1',
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面加载，接收上一个界面传递过来的参数，并通过wx.setNavigationBarTitle来设置导航标题
    */
-  onLoad: function (options) {
+  onLoad: function (e) {
+    var startStation=e.startStation;//始发站
+    var endStation=e.endStation;//终点站
+    var date=e.date;//日期
+    console.log("startStation="+startStation+"---endStation="+endStation+"---date="+date);
+    wx.setNavigationBarTitle({
+      title: startStation+'→'+endStation
+    });
 
+    /**
+     * 定义一个date日期变量，把传递过来的日期赋值给date变量
+     */
+    this.setData({date:date});
+    
+    this.loadTrainsList(startStation,endStation);
+  },
+  loadTrainsList:function(startStation,endStation){
+    var page=this;
+    wx.request({
+      url: 'https://apis.juhe.cn/train/s2swithprice',
+      data:{
+        start:startStation,//始发站
+        end:endStation,//终点站
+        date:'',//日期，若不填，默认查询明天的火车票信息
+        dtype:'',//火车类型
+        key:'5a54c3ff25d0b336ff7dad3d4e889c65'
+      },
+      method:'GET',
+      success:function(res){
+        console.log(res);
+        var trainList=res.data.result.list;
+        var trainList=wx.getStorageSync('trainList');
+        var size=trainList.length;
+        var winHeight=size*100+30;
+        page.setData({trainList:trainList});
+        page.setData({winHeight:winHeight});
+      }
+    });
+  },
+  /**
+   * switchNav菜单来切换事件,动态设置页签导航选中效果
+   */
+  switchNav:function(e){
+    var id=e.currentTarget.id;
+    console.log(id);
+    this.setData({currentTab:id});
   },
 
   /**
